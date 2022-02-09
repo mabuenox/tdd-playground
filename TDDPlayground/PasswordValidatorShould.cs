85f06fe7943130edcf643319afd23defc78935b6
Miguel Ángel Bueno Ferrer <miguel.bueno@analyticalways.com>
2022-02-09 16:36:45 +0100

    red: PasswordValidatorShould_verify_password_without_a_number_is_invalid

--- TDDPlayground/PasswordValidatorShould.cs
+++ TDDPlayground/PasswordValidatorShould.cs
@@ -7,7 +7,7 @@ namespace TDDPlayground
     public class PasswordValidatorShould
     {
 
-        // - [ ] La contraseña debe tener como minimo 8 caracteres
+        // - [x] La contraseña debe tener como minimo 8 caracteres
         // - [ ] La contraseña debe tener al menos un nùmero.
         // - [ ] La contraseña debe tener al menos letra en mayuscula.
         // - [ ] La contraseña debe tener al menos letra en minuscula.
@@ -28,6 +28,18 @@ namespace TDDPlayground
         public void verify_password_with_least_of_8_character_is_invalid()
         {
             string password = "pass";
+
+            var validator = new PasswordValidator();
+
+            bool result = validator.Verify(password);
+
+            result.Should().BeFalse();
+        }
+
+        [Fact]
+        public void verify_password_without_a_number_is_invalid()
+        {
+            string password = "password";
 
             var validator = new PasswordValidator();
 
